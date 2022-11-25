f83fb89df0d613fc2539c5417e585ee66fc13006
Luiz Dorça <luizdorca@gmail.com>
2022-11-24 23:39:30 -0300

    added GetMapping

--- src/main/java/br/com/mentorama/Mod01Atividade/CadastroAlunosController.java
+++ src/main/java/br/com/mentorama/Mod01Atividade/CadastroAlunosController.java
@@ -4,15 +4,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
-@RequestMapping("/olamundo")
+@RequestMapping("/alunos")
 public class CadastroAlunosController {
 
-    private final String message = "Olá Mundo";
+    private final List<Aluno> listaAlunos;
+
+    // Construtor para definir e inicializar a lista de alunos:
+    public CadastroAlunosController() {
+        this.listaAlunos = new ArrayList<>();
+        this.listaAlunos.add(new Aluno(1, "João",30));
+        this.listaAlunos.add(new Aluno(2, "Maria",45));
+        this.listaAlunos.add(new Aluno(3, "José",58));
+    }
+
 
     @GetMapping
-    public String olaMundo() {
-        return message;
+    public List<Aluno> findAll() {
+        return listaAlunos;
     }
 
 }
